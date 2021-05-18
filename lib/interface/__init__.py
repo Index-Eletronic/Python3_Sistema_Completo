5850d0b344fa850eec7493c10dd009628c17caa8
Fil Tuon <index.eletronic@gmail.com>
2021-05-17 21:00:58 -0300

    .

--- lib/interface/__init__.py
+++ lib/interface/__init__.py
@@ -1,3 +1,17 @@
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\33[31m ERRO: Por favor digite uma das opções válidas\33[m')
+            continue
+        except (KeyboardInterrupt):
+            print('O Usuário prefereiu não digitar o dado')
+            return 0
+        else:
+            return n
+
+
 def linha(tam = 42):
     return '-' * tam
 
@@ -10,6 +24,11 @@ def menu(lista):
     cabeçalho('MENU PRINCIPAL')
     c = 1
     for item in lista:
-        print(f'{c} - {item}')
+        print(f'\33[33m{c}\33[m - \33[34m{item}\33[m')
         c +=1
     print(linha())
+    opc = leiaInt('\33[32mSua Opção:\33[m ')
+    return opc
+
+
+
--- lib/sistema.py
+++ lib/sistema.py
@@ -1,3 +1,15 @@
 from lib.interface import *
+from time import sleep
 
-menu(['opc01', 'opc02', 'op03'])
+while True:
+    resposta = menu(['Ver pessoas Cadastradas', 'Cadastrar Nova Pessoa', 'Sair do Sistema'])
+    if resposta == 1:
+        cabeçalho('Opção 1')
+    elif resposta == 2:
+        cabeçalho('Opção 2')
+    elif resposta == 3:
+        cabeçalho('Saindo do Sistema ... Até Logo!')
+        break
+    else:
+        print('\33[31mErro! Digite uma das opções válidas\33[m')
+    sleep(2)
